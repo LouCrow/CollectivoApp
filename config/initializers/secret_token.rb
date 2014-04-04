7a35ee647ccb05bc3dde81834fdc9f89e8efcc2f
Casey Bailey <casey@exosphe.re>
2014-04-04 15:52:17 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CollectivoApp::Application.config.secret_key_base = '8720a348e525c25b6bb32eb34aa904636162fa7bce9f2908089daafd8e5077716f0c5092c41e2def3f5c0b28da5219942e57ae69c3e1523954a740fac6c80b02'
